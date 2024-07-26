8856ca269c6e541509e1ac37fa1961b7b49fa8fe
Anton Kovalchuk <koval400k@gmail.com>
2024-07-26 14:58:05 +0200

    fix cross-chain-relayer

--- scripts/optimism/cross-chain-relayer.ts
+++ scripts/optimism/cross-chain-relayer.ts
@@ -4,6 +4,8 @@ import network from "../../utils/network";
 import addresses from "../../utils/optimism/addresses";
 import { Bytes } from "@ethersproject/bytes";
 import testing from "../../utils/testing";
+import { wei } from "../../utils/wei";
+import { L2CrossDomainMessenger__factory } from "../../typechain";
 
 // 1. monitor L1 for TransactionDeposited event
 // 2. decode calldata
@@ -19,8 +21,34 @@ async function main() {
   const ethProviderUrl = 'ws://localhost:8545';
   const wsEthProvider = new ethers.providers.WebSocketProvider(ethProviderUrl);
 
+  const l1CrossDomainMessengerAliased = await testing.impersonate(
+    testing.accounts.applyL1ToL2Alias(optAddresses.L1CrossDomainMessenger),
+    optProvider
+  );
+  console.log('l1CrossDomainMessengerAliased=', l1CrossDomainMessengerAliased);
+
+  await testing.setBalance(
+    await l1CrossDomainMessengerAliased.getAddress(),
+    wei.toBigNumber(wei`1 ether`),
+    optProvider
+  );
+
   // 1. Catch Event
   const optimismPortalAddress = "0x16Fc5058F25648194471939df75CF27A2fdC48BC";
+
+  const l1MessngerAbi = [
+    "event SentMessage(address indexed target, address sender, bytes message, uint256 messageNonce, uint256 gasLimit)"
+  ];
+  const contractZ = new Contract(optAddresses.L1CrossDomainMessenger, l1MessngerAbi, wsEthProvider);
+  contractZ.on('SentMessage', (target, sender, message, messageNonce, gasLimit) => {
+    console.log('SentMessage event triggered:', {
+      target: target,
+      sender: sender,
+      message: message.toString(),
+      messageNonce: messageNonce,
+      gasLimit: gasLimit
+    });
+  });
 
   const l1OptimismPortalAbi = [
     "event TransactionDeposited(address indexed from, address indexed to, uint256 indexed version, bytes opaqueData)"
@@ -45,17 +73,39 @@ async function main() {
     const dataToSend = ethers.utils.hexDataSlice(opaqueDataBytes, dataOffset, dataOffset + txDataLen);
     console.log('dataToSend=', dataToSend);
 
-    // 3. Send data
-    const l1CrossDomainMessengerAliased = testing.accounts.applyL1ToL2Alias(optAddresses.L1CrossDomainMessenger);
-    console.log('l1CrossDomainMessengerAliased=', l1CrossDomainMessengerAliased);
+    const relayMessageInterface = new ethers.utils.Interface(['function relayMessage(uint256 _nonce,address _sender,address _target,uint256 _value,uint256 _minGasLimit,bytes calldata _message)']);
+    const decodedRelayMessageArgs = relayMessageInterface.decodeFunctionData('relayMessage', dataToSend)
+    console.log('relayMessage function decodedArgs=', decodedRelayMessageArgs);
 
-    const txArg = {
-      to: optAddresses.L2CrossDomainMessenger.toLowerCase(),
-      from: l1CrossDomainMessengerAliased.toLowerCase(),
-      data: dataToSend,
-      gasLimit: ethers.utils.hexlify(1000000)
-    }
-    optProvider.getSigner(l1CrossDomainMessengerAliased).sendTransaction(txArg);
+    const _target = decodedRelayMessageArgs['_target'];
+    const _sender = decodedRelayMessageArgs['_sender'];
+    const _value = decodedRelayMessageArgs['_value'];
+    const _nonce = decodedRelayMessageArgs['_nonce'];
+    const _minGasLimit = 300_000;
+    const _message = decodedRelayMessageArgs['_message'];
+
+    const queueInterface = new ethers.utils.Interface(['function queue(address[] memory targets, uint256[] memory values,string[] memory signatures,bytes[] memory calldatas,bool[] memory withDelegatecalls)']);
+    const decodedQueueArgs = queueInterface.decodeFunctionData('queue', _message)
+    console.log('queue function decodedArgs=', decodedQueueArgs);
+
+
+    // 3. Send data
+    const l2CrossDomainMessenger = L2CrossDomainMessenger__factory.connect(
+      optAddresses.L2CrossDomainMessenger,
+      optProvider
+    );
+    void (async () => {
+      const tx = await l2CrossDomainMessenger.connect(l1CrossDomainMessengerAliased).relayMessage(
+        _nonce,
+        _sender,
+        _target,
+        _value,
+        _minGasLimit,
+        _message,
+        { gasLimit: 5_000_000 }
+      );
+      console.log("tx=",tx);
+    })();
   });
 
   // 4. Listen to L2 RelayedMessage event
@@ -69,6 +119,11 @@ async function main() {
   const contractM = new Contract(optAddresses.L2CrossDomainMessenger, messengerAbi, wsOptProvider);
   contractM.on('RelayedMessage', (target) => {
     console.log('RelayedMessage event triggered:', {
+      target: target
+    });
+  });
+  contractM.on('FailedRelayedMessage', (target) => {
+    console.log('FailedRelayedMessage event triggered:', {
       target: target
     });
   });
